42cab6b286d4e74acf72193f72992e345fb4a8cb
alexiscarrere <alexis.carrere12@gmail.com>
2023-02-14 09:24:04 +0100

    New activity

--- app/src/main/java/com/example/testcda/MainActivity.kt
+++ app/src/main/java/com/example/testcda/MainActivity.kt
@@ -4,18 +4,33 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //Add button
         val button = findViewById<Button>(R.id.start_new_activity_btn)
         button.setOnClickListener{
             val intent = Intent(this, NewActivity::class.java)
+            //add action
+            intent.action = Intent.ACTION_VIEW
+            //ADD CATEGORY
+            intent.addCategory("user")
+            // ADD EXTRA
+            intent.putExtra("name", "Alexis")
+            intent.putExtra("age", 27)
             startActivity(intent)
         }
+
+        // Log
+        Log.v("MainActivity", "Verbose message")
+        Log.d("MainActivity", "Debug message")
+
     }
 }
--- app/src/main/java/com/example/testcda/NewActivity.kt
+++ app/src/main/java/com/example/testcda/NewActivity.kt
@@ -2,10 +2,24 @@ package com.example.testcda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class NewActivity : AppCompatActivity() {
+    val TAG = "NewActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
+
+        //Definir mon action
+        var action = intent.action
+
+        //categorie
+        val isUserView = intent.hasCategory("user")
+        //charger mes extras
+        val extras: Bundle? = intent.extras
+        val name = extras?.getString("name")
+        val age = extras?.getInt("age")
+
+        Log.i(TAG, "ation : $action, isUserView : $isUserView, name : $name, age : $age")
     }
 }
